2d63a1b5aff4ed691a3b665000a7a67eb8c212c5
Kevin Faustino Subias Gutiérrez <124650275+kewinsito@users.noreply.github.com>
2024-11-16 12:12:04 -0700

    menu

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function autoSlide() {
 
 // Cambiar de imagen cada 3 segundos
 setInterval(autoSlide, 3000);
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtener todos los enlaces de navegación
+    const links = document.querySelectorAll('.nav-link');
+  
+    // Agregar el evento de clic a cada enlace
+    links.forEach(link => {
+      link.addEventListener('click', function() {
+        // Eliminar la clase 'active' de todos los enlaces
+        links.forEach(link => link.classList.remove('active'));
+        // Añadir la clase 'active' al enlace clickeado
+        this.classList.add('active');
+      });
+    });
+  });
